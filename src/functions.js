26c059b5fa1639e2e9e5c75126d444bbc429be0a
ayman-benammour <benammourayman@gmail.com>
2023-10-18 15:53:32 +0200

    :sparkles: feat/sqlcraft-0005/Creation of several basic functions (SELECT, UPDATE, DELETE)

--- src/functions.js
+++ src/functions.js
@@ -1,1 +1,37 @@
-// Functions
+export const sqlCraft = {
+    SELECT: function(table, columns, condition) {
+        let query = "SELECT ";
+
+        if (columns && columns.length > 0) {
+            query += columns.join(", ");
+        } else {
+            query += "*";
+        }
+
+        query += " FROM " + table;
+
+        if (condition) {
+            query += " WHERE " + condition;
+        }
+
+        return query;
+    },
+    DELETE: function(table, condition) {
+        let query = "DELETE FROM " + table;
+
+        if (condition) {
+            query += " WHERE " + condition;
+        }
+
+        return query;
+    },
+    UPDATE: function(table, updates, condition) {
+        let query = "UPDATE " + table + " SET ";
+
+        if (condition) {
+            query += " WHERE " + condition;
+        }
+
+        return query;
+    }
+};
