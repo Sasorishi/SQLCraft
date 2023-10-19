ba9957fd024261e594208864390a541de052fad9
ayman-benammour <benammourayman@gmail.com>
2023-10-19 11:44:17 +0200

    :bug: fix/sqlcraft-0015/Adding semicolons at each end of functions, and adjusting the SELECT function

--- src/functions.js
+++ src/functions.js
@@ -16,22 +16,29 @@ function deleteDatabase(name) {
 }
 
 export const sqlCraft = {
-    SELECT: function(table, columns, condition) {
+    SELECT: function(columns, table, condition) {
         let query = "SELECT ";
 
         if (columns && columns.length > 0) {
-            query += columns.join(", ");
+            if (columns === "all") {
+                query += "*";
+            }
+            else {
+                query += columns;
+            }
         } else {
             query += "*";
         }
 
-        query += " FROM " + table;
+        if (table && table.length > 0) {
+            query += ` FROM ${table}`;
+        }
 
         if (condition) {
             query += " WHERE " + condition;
         }
 
-        return query;
+        return query + ";";
     },
     DELETE: function(table, condition) {
         let query = "DELETE FROM " + table;
@@ -40,7 +47,7 @@ export const sqlCraft = {
             query += " WHERE " + condition;
         }
 
-        return query;
+        return query + ";";
     },
     UPDATE: function(table, updates, condition) {
         let query = "UPDATE " + table + " SET " + updates
@@ -49,6 +56,6 @@ export const sqlCraft = {
             query += " WHERE " + condition;
         }
 
-        return query;
+        return query + ";";
     }
 };
